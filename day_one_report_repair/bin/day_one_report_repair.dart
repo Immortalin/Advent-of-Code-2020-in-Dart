fadd7e88bf701bc6545b66c33da0e2c4ff3c3c93
Immortalin <7126128+Immortalin@users.noreply.github.com>
2021-04-27 22:50:35 -0400

    Fiat lux🌟

---
+++ day_one_report_repair/bin/day_one_report_repair.dart
@@ -0,0 +1,5 @@
+import 'package:day_one_report_repair/day_one_report_repair.dart' as day_one_report_repair;
+
+void main(List<String> arguments) {
+  print('Hello world: ${day_one_report_repair.calculate()}!');
+}
---
+++ day_one_report_repair/test/day_one_report_repair_test.dart
@@ -0,0 +1,8 @@
+import 'package:day_one_report_repair/day_one_report_repair.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
