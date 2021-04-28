ba752ca1a716f5affb28394a6cd4913f02a85c59
Immortalin <7126128+Immortalin@users.noreply.github.com>
2021-04-28 14:04:44 -0400

    Add day 3

---
+++ day_three_toboggan_trajectory/bin/day_three_toboggan_trajectory.dart
@@ -0,0 +1,5 @@
+import 'package:day_three_toboggan_trajectory/day_three_toboggan_trajectory.dart' as day_three_toboggan_trajectory;
+
+void main(List<String> arguments) {
+  print('Hello world: ${day_three_toboggan_trajectory.calculate()}!');
+}
---
+++ day_three_toboggan_trajectory/test/day_three_toboggan_trajectory_test.dart
@@ -0,0 +1,8 @@
+import 'package:day_three_toboggan_trajectory/day_three_toboggan_trajectory.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
