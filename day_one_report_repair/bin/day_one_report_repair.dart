17135e9ca5293b625996ceaa8b025437f4f69e52
Immortalin <7126128+Immortalin@users.noreply.github.com>
2021-04-27 23:14:26 -0400

    Day one complete

--- day_one_report_repair/bin/day_one_report_repair.dart
+++ day_one_report_repair/bin/day_one_report_repair.dart
@@ -1,5 +1,32 @@
-import 'package:day_one_report_repair/day_one_report_repair.dart' as day_one_report_repair;
+import 'package:day_one_report_repair/day_one_report_repair.dart'
+    as day_one_report_repair;
 
-void main(List<String> arguments) {
-  print('Hello world: ${day_one_report_repair.calculate()}!');
+import 'dart:io';
+
+void main(List<String> arguments) async {
+  const total = 2020;
+  var lines = await File('puzzle_input.txt').readAsLines();
+  var values = <int>[];
+  for (var line in lines) {
+    values.add(int.parse(line));
+  }
+  int current_variable;
+  int second_variable;
+  outerLoop:
+  for (var i = 0; i < values.length; i++) {
+    current_variable = values[i];
+    var difference = total - current_variable;
+    for (var j = i + 1; j < values.length; j++) {
+      var next = values[j];
+      if (next == difference) {
+        second_variable = next;
+        break outerLoop;
+      }
+    }
+  }
+
+  print(second_variable * current_variable);
+  // print(values);
+  // print('Hello world: ${day_one_report_repair.calculate()}!');
+  //
 }
