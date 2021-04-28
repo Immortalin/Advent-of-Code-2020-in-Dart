8bd44420e53c78fbca8c5530c5fd85f4da764eaa
Immortalin <7126128+Immortalin@users.noreply.github.com>
2021-04-28 13:45:19 -0400

    Day 2 complete

--- day_two_password_philosophy/bin/day_two_password_philosophy.dart
+++ day_two_password_philosophy/bin/day_two_password_philosophy.dart
@@ -2,19 +2,18 @@ import 'dart:io';
 
 // import 'package:day_two_password_philosophy/day_two_password_philosophy.dart'
 //     as day_two_password_philosophy;
-
-void main(List<String> arguments) async {
+void valid_password_part_one() async {
   var lines = await File('puzzle_input.txt').readAsLines();
   var valid = 0;
   for (var line in lines) {
     var items = line.split(' ');
 
     var target_range = items[0].split('-');
-    print(target_range);
+    // print(target_range);
     var target_char = items[1].split(':')[0];
-    print(target_char);
+    // print(target_char);
     var target_string = items[2];
-    print(target_string);
+    // print(target_string);
     var upper = int.parse(target_range[1]);
     var lower = int.parse(target_range[0]);
     // var exp = RegExp('{${target_range[0]}, ${target_range[1]}}');
@@ -30,5 +29,33 @@ void main(List<String> arguments) async {
     }
   }
   print('Valid passwords: $valid');
+}
+
+void valid_password_part_two() async {
+  var lines = await File('puzzle_input.txt').readAsLines();
+  var valid = 0;
+  for (var line in lines) {
+    var items = line.split(' ');
+
+    var target_range = items[0].split('-');
+    // print(target_range);
+    var target_char = items[1].split(':')[0];
+    // print(target_char);
+    var target_string = items[2];
+    // print(target_string);
+    var upper = int.parse(target_range[1]);
+    var lower = int.parse(target_range[0]);
+    // https://api.flutter.dev/flutter/dart-core/bool/operator_bitwise_exclusive_or.html
+    if ((target_string[lower - 1] == target_char) ^
+        (target_string[upper - 1] == target_char)) {
+      valid += 1;
+    }
+  }
+  print('Valid passwords: $valid');
+}
+
+void main(List<String> arguments) async {
+  valid_password_part_one();
+  valid_password_part_two();
   // print('Hello world: ${day_two_password_philosophy.calculate()}!');
 }
