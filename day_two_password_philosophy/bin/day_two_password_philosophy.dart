0320d3ab8a7f2a2fca9857fe5399c49cc3884d89
Immortalin <7126128+Immortalin@users.noreply.github.com>
2021-04-28 10:49:19 -0400

    password validity part 1 complete

--- day_two_password_philosophy/bin/day_two_password_philosophy.dart
+++ day_two_password_philosophy/bin/day_two_password_philosophy.dart
@@ -1,5 +1,34 @@
-import 'package:day_two_password_philosophy/day_two_password_philosophy.dart' as day_two_password_philosophy;
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${day_two_password_philosophy.calculate()}!');
+// import 'package:day_two_password_philosophy/day_two_password_philosophy.dart'
+//     as day_two_password_philosophy;
+
+void main(List<String> arguments) async {
+  var lines = await File('puzzle_input.txt').readAsLines();
+  var valid = 0;
+  for (var line in lines) {
+    var items = line.split(' ');
+
+    var target_range = items[0].split('-');
+    print(target_range);
+    var target_char = items[1].split(':')[0];
+    print(target_char);
+    var target_string = items[2];
+    print(target_string);
+    var upper = int.parse(target_range[1]);
+    var lower = int.parse(target_range[0]);
+    // var exp = RegExp('{${target_range[0]}, ${target_range[1]}}');
+    var count = 0;
+    for (var char in target_string.split('')) {
+      if (char == target_char) {
+        count += 1;
+      }
+    }
+
+    if (lower <= count && count <= upper) {
+      valid += 1;
+    }
+  }
+  print('Valid passwords: $valid');
+  // print('Hello world: ${day_two_password_philosophy.calculate()}!');
 }
