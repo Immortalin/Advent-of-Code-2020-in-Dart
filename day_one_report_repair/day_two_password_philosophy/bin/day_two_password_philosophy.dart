71b39b73be7e78ab69ddf7c6823bff7e0d79eb6c
Immortalin <7126128+Immortalin@users.noreply.github.com>
2021-04-27 23:56:23 -0400

    Add day two puzzle

---
+++ day_one_report_repair/day_two_password_philosophy/bin/day_two_password_philosophy.dart
@@ -0,0 +1,5 @@
+import 'package:day_two_password_philosophy/day_two_password_philosophy.dart' as day_two_password_philosophy;
+
+void main(List<String> arguments) {
+  print('Hello world: ${day_two_password_philosophy.calculate()}!');
+}
---
+++ day_one_report_repair/day_two_password_philosophy/test/day_two_password_philosophy_test.dart
@@ -0,0 +1,8 @@
+import 'package:day_two_password_philosophy/day_two_password_philosophy.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
