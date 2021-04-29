e23c912de87c76ddbe7abdea8a2b542643cef314
Immortalin <7126128+Immortalin@users.noreply.github.com>
2021-04-29 13:45:56 -0400

    Complete part 2, update isolates library

--- day_three_toboggan_trajectory/bin/day_three_toboggan_trajectory.dart
+++ day_three_toboggan_trajectory/bin/day_three_toboggan_trajectory.dart
@@ -16,12 +16,42 @@ int rollover(int n, int size, int step_size) {
   return 0;
 }
 
-/// Wraparound an 0-indexed integer [n] of max value 30 (31-1) using the
-/// standard modulo algorithm incrementing by 3
+/// Traverses toboggan slope with [n] right steps and [m] down steps
+/// and returns number of [trees]
 ///
-@pragma('vm:prefer-inline')
-int rollover_31(int n) {
-  return rollover(n, 31, 3);
+int right_n_down_m(List<String> lines, int n, int m) {
+  var counter = 0;
+  var trees = 0;
+
+  for (var i = 0; i < lines.length; i += m) {
+    var line = lines[i];
+    // print(line[counter]);
+    if (line[counter] == '#') {
+      trees += 1;
+    }
+    counter = rollover(counter, 31, n);
+  }
+  return trees;
+}
+
+int right_three_down_one(List<String> lines) {
+  return right_n_down_m(lines, 3, 1);
+}
+
+int right_one_down_one(List<String> lines) {
+  return right_n_down_m(lines, 1, 1);
+}
+
+int right_five_down_one(List<String> lines) {
+  return right_n_down_m(lines, 5, 1);
+}
+
+int right_seven_down_one(List<String> lines) {
+  return right_n_down_m(lines, 7, 1);
+}
+
+int right_one_down_two(List<String> lines) {
+  return right_n_down_m(lines, 1, 2);
 }
 
 void main(List<String> arguments) {
@@ -31,16 +61,24 @@ void main(List<String> arguments) {
   // i = rollover_13(i);
   // print(i);
   //
-  var counter = 0;
-  var trees = 0;
   var lines = File('puzzle_input.txt').readAsLinesSync();
-  for (var line in lines) {
-   // print(line[counter]);
-    if (line[counter] == '#') {
-      trees += 1;
-    }
-    counter = rollover_31(counter);
-  }
-  print('Trees found: $trees');
+
+  var right_1_down_1_trees = right_one_down_one(lines);
+  print('Trees found with right 1 down 1: $right_1_down_1_trees');
+
+  var right_3_down_1_trees = right_three_down_one(lines);
+  print('Trees found with right 3 down 1: $right_3_down_1_trees');
+
+  var right_5_down_1_trees = right_five_down_one(lines);
+  print('Trees found with right 5 down 1: $right_5_down_1_trees');
+
+  var right_7_down_1_trees = right_seven_down_one(lines);
+  print('Trees found with right 7 down 1: $right_7_down_1_trees');
+
+  var right_1_down_2_trees = right_one_down_two(lines);
+  print('Trees found with right 1 down 2: $right_1_down_2_trees');
   // print('Hello world: ${day_three_toboggan_trajectory.calculate()}!');
+  //
+  print(
+      'Product of trees: ${right_1_down_1_trees * right_3_down_1_trees * right_5_down_1_trees * right_7_down_1_trees * right_1_down_2_trees}');
 }
